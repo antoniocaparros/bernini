f5cc839a2bb23cd1887d5173ebb62f8e8406cf5d
Miguel Ángel García Gandía <mgarcia0094@gmail.com>
2021-05-28 01:13:09 +0200

    initial_Data

--- bernini/start.sh
+++ bernini/start.sh
@@ -1,4 +1,6 @@
 python manage.py makemigrations
 python manage.py migrate
-python manage.py loaddata
+python manage.py loaddata initial_data/user.json
+python manage.py loaddata initial_data/products.json
+python manage.py loaddata initial_data/carts.json
 python manage.py runserver 0.0.0.0:8000
